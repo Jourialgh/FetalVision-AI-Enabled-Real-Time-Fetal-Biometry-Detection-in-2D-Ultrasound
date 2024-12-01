481c54e29437c7fe4a0235873e3456f91f8102aa
shadanyousef <120052739+shadanyousef@users.noreply.github.com>
2024-12-01 15:07:08 +0300

    Add files via upload

---
+++ pyqt_demoF-3/view/patientwindow.cpp
@@ -0,0 +1,14 @@
+#include "patientwindow.h"
+#include "ui_patientwindow.h"
+
+PatientWindow::PatientWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::PatientWindow)
+{
+    ui->setupUi(this);
+}
+
+PatientWindow::~PatientWindow()
+{
+    delete ui;
+}
